452c95c66746d00074227a0933a448d423291709
chialin <chialin.shr@gmail.com>
2024-10-03 15:32:34 +0800

    Init add expense page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { assetAccounts } from './constants/assetAccounts'; // 資產帳號
 import { expenses } from './constants/expenses'; // 解析出的花費資料
 import { Expense } from './types/expense';
+import styles from './styles/ExpenseManager.module.css';
 
 function App() {
   const [data, setData] = useState<Expense[]>([]);
@@ -30,73 +31,89 @@ function App() {
   const exportToday = () => {};
 
   return (
-    <div>
-      <h1>每日花費管理</h1>
-      <button onClick={exportToday}>Export Today</button>
+    <div className="container">
+      <h1>每日花費紀錄</h1>
 
       <h2>新增花費</h2>
-      <div>
-        <label>日期：</label>
-        <input
-          type="date"
-          value={date}
-          onChange={(e) => setDate(e.target.value)}
-        />
+      <div className="row mb-3">
+        <label className="col-12">日期：</label>
+        <div className="col-12">
+          <input
+            type="date"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+            className={`${styles.formControl} form-control`}
+          />
+        </div>
       </div>
-      <div>
-        <label>帳號：</label>
-        <select value={account} onChange={(e) => setAccount(e.target.value)}>
-          {assetAccounts.map((acc, index) => (
-            <option key={index} value={acc}>
-              {acc}
-            </option>
-          ))}
-        </select>
-      </div>
-      <div>
-        <label>花費類型：</label>
-        <select
-          value={expenseType}
-          onChange={(e) => setExpenseType(e.target.value)}
-        >
-          {expenses.map((type, index) => (
-            <option key={index} value={type}>
-              {type}
-            </option>
-          ))}
-        </select>
-      </div>
-      <div>
-        <label>金額：</label>
-        <input
-          type="number"
-          onChange={(e) => setAmount(Number(e.target.value))}
-        />
-      </div>
-      <button onClick={addExpense}>新增花費</button>
-
-      <h2>今日花費</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>日期</th>
-            <th>帳號</th>
-            <th>花費類型</th>
-            <th>金額</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data &&
-            data.map((entry: Expense, index) => (
-              <tr key={index}>
-                <td>{entry.date}</td>
-                <td>{entry.account}</td>
-                <td>{entry.expenseType}</td>
-                <td>{entry.amount}</td>
-              </tr>
+      <div className="row mb-3">
+        <label className="col-12">帳號：</label>
+        <div className="col-12">
+          <select
+            value={account}
+            onChange={(e) => setAccount(e.target.value)}
+            className={`${styles.formControl} form-select`}
+          >
+            {assetAccounts.map((acc, index) => (
+              <option key={index} value={acc}>
+                {acc}
+              </option>
             ))}
-        </tbody>
-      </table>
+          </select>
+        </div>
+      </div>
+      <div className="row mb-3">
+        <label className="col-12">花費類型：</label>
+        <div className="col-12">
+          <select
+            value={expenseType}
+            onChange={(e) => setExpenseType(e.target.value)}
+            className={`${styles.formControl} form-select`}
+          >
+            {expenses.map((type, index) => (
+              <option key={index} value={type}>
+                {type}
+              </option>
+            ))}
+          </select>
+        </div>
+      </div>
+      <div className="row mb-3">
+        <label className="col-12">金額：</label>
+        <div className="col-12">
+          <input
+            type="number"
+            onChange={(e) => setAmount(Number(e.target.value))}
+            className={`${styles.formControl} form-control`}
+          />
+        </div>
+      </div>
+      <button
+        onClick={addExpense}
+        className={`${styles.btn} ${styles.btnPrimary}`}
+      >
+        新增花費
+      </button>
+      <button
+        onClick={exportToday}
+        className={`${styles.btn} ${styles.btnPrimary}`}
+      >
+        匯出今日花費
+      </button>
+      <h2>今日花費</h2>
+      <div className={styles.expenseList}>
+        {data &&
+          data.map((entry: Expense, index) => (
+            <div key={index} className={`${styles.expenseItem} row`}>
+              <span className={styles.date}>{entry.date}</span>
+              <div className={styles.row}>
+                <span className={styles.account}>{entry.account}</span>
+                <span className={styles.expenseType}>{entry.expenseType}</span>
+              </div>
+              <span className={styles.money}>{entry.amount}</span>
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
