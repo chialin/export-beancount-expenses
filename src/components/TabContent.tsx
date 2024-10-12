8a3edc181f7b533eb6d5eeea7d16d9d11286eed3
chialin <chialin.shr@gmail.com>
2024-10-12 16:28:45 +0800

    Revised last 3 tabs to menu item

--- src/components/TabContent.tsx
+++ src/components/TabContent.tsx
@@ -1,9 +1,21 @@
-import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react';
+import {
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+  IconButton,
+} from '@chakra-ui/react';
+import { Menu, MenuButton, MenuList, MenuItem } from '@chakra-ui/react';
+import { GiHamburgerMenu } from 'react-icons/gi';
 import ExpenseForm from './ExpenseForm';
 import ExpenseList from './ExpenseList';
 import TransferForm from './TransferForm';
-import Settings from './CreditCardSettings';
+import CreditCardSettings from './CreditCardSettings';
+import BankSettings from './BankSettings';
+
 import { Expense } from '../types/expense';
+import { useState } from 'react';
 
 interface TabContentProps {
   data: Expense[];
@@ -20,36 +32,67 @@ const TabContent: React.FC<TabContentProps> = ({
   onClear,
   onTransfer,
 }) => {
-  // const [transactions, setTransactions] = useState<string[]>([]);
+  const [tabIndex, setTabIndex] = useState(0);
 
   const handleTransfer = (transaction: string) => {
     onTransfer(transaction);
   };
 
-  return (
-    <Tabs variant="soft-rounded" colorScheme="green">
-      <TabList>
-        <Tab>每日花費</Tab>
-        <Tab>轉帳功能</Tab>
-        <Tab>今日交易明細</Tab>
-        <Tab>信用卡設定</Tab>
-      </TabList>
+  const handleMenuItemClick = (index: number) => {
+    setTabIndex(index);
+  };
 
-      <TabPanels>
-        <TabPanel>
-          <ExpenseForm onAddExpense={onAddExpense} />
-        </TabPanel>
-        <TabPanel>
-          <TransferForm onTransfer={handleTransfer} />
-        </TabPanel>
-        <TabPanel>
-          <ExpenseList data={data} onExport={onExport} onClear={onClear} />
-        </TabPanel>
-        <TabPanel>
-          <Settings />
-        </TabPanel>
-      </TabPanels>
-    </Tabs>
+  return (
+    <>
+      <Tabs
+        variant="soft-rounded"
+        colorScheme="green"
+        index={tabIndex}
+        onChange={setTabIndex}
+      >
+        <TabList>
+          <Menu autoSelect={false}>
+            <MenuButton
+              as={IconButton}
+              aria-label="Options"
+              icon={<GiHamburgerMenu />}
+              variant="outline"
+            />
+            <MenuList>
+              <MenuItem onClick={() => handleMenuItemClick(2)}>
+                消費明細
+              </MenuItem>
+              <MenuItem onClick={() => handleMenuItemClick(3)}>
+                信用卡設定
+              </MenuItem>
+              <MenuItem onClick={() => handleMenuItemClick(4)}>
+                銀行帳號設定
+              </MenuItem>
+            </MenuList>
+          </Menu>
+          <Tab ml={2}>每日花費</Tab>
+          <Tab>轉帳功能</Tab>
+        </TabList>
+
+        <TabPanels>
+          <TabPanel>
+            <ExpenseForm onAddExpense={onAddExpense} />
+          </TabPanel>
+          <TabPanel>
+            <TransferForm onTransfer={handleTransfer} />
+          </TabPanel>
+          <TabPanel>
+            <ExpenseList data={data} onExport={onExport} onClear={onClear} />
+          </TabPanel>
+          <TabPanel>
+            <CreditCardSettings />
+          </TabPanel>
+          <TabPanel>
+            <BankSettings />
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </>
   );
 };
 
